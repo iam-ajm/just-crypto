892dd0e7458ad13ab912b1c7b90f866207ad5409
JM Arenas <jhorene.arenas@gmail.com>
2022-01-04 15:21:23 +0800

    Send request to coingecko api for current crypto listing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,8 @@
 class HomeController < ApplicationController
   def index
-    @api_key = ENV["MY_API_KEY"]
-    @url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
-    @symbol = self.symbol
-    @query = {
-        "symbol" => @symbol
-    }
-    @headers = {
-        "X-CMC_PRO_API_KEY" => @api_key,
-        "Accepts" => "application/json"
-    }
-    @request = HTTParty.get(@url, :headers => @headers, :query => @query)
-    @response = JSON.parse(@request.body)["data"][@symbol]["quote"]["USD"]["price"]
+    @url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd"
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
     @coins = JSON.parse(@response)
   end
 
