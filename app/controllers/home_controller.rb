69b22c1df50663cbb2e28eff9bede5b96a392414
JM Arenas <jhorene.arenas@gmail.com>
2022-01-04 15:04:57 +0800

    Send request to coinmarket api for latest crypto

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,18 @@
 class HomeController < ApplicationController
   def index
-    @hello = "Hello there!"
+    @api_key = ENV["MY_API_KEY"]
+    @url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
+    @symbol = self.symbol
+    @query = {
+        "symbol" => @symbol
+    }
+    @headers = {
+        "X-CMC_PRO_API_KEY" => @api_key,
+        "Accepts" => "application/json"
+    }
+    @request = HTTParty.get(@url, :headers => @headers, :query => @query)
+    @response = JSON.parse(@request.body)["data"][@symbol]["quote"]["USD"]["price"]
+    @coins = JSON.parse(@response)
   end
 
   def about
